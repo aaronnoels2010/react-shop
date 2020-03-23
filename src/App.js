b2953cc29d7ecbf2207e1dd97a94d23006edb26c
aaronnoels <noels.aaron@gmail.com>
2020-03-23 19:26:14 +0100

    Setup project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom"
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import HatsPage from "./pages/hatspage/hatspage.component";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/hats" component={HatsPage}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
